bf691cc9531165521a105157d171531cd5d30eec
Hamsika Krishnan Rapolu <97187233+Hamsikakrishnan@users.noreply.github.com>
2025-03-02 16:47:28 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import {getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD98cmFCdQ1EYkm_7vQ8HF0dq9l0t89dFY",
-  authDomain: "inventory-management-ff2e2.firebaseapp.com",
-  projectId: "inventory-management-ff2e2",
-  storageBucket: "inventory-management-ff2e2.appspot.com",
-  messagingSenderId: "403095039359",
-  appId: "1:403095039359:web:a301a9c7b5db85540bfe4a",
-  measurementId: "G-Y35HF4C8SE"
+  apiKey: #,
+  authDomain:#
+  projectId: #,
+  storageBucket: #,
+  messagingSenderId:#,
+  appId:#,
+  measurementId: #
 };
 
 // Initialize Firebase
